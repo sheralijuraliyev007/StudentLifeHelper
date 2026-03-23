d9636b50f9e988d91fccd12782e79571e8c17c85
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-23 23:48:15 +0900

    Info Room Type Added

---
+++ StudentLifeHelper.Data/Entities/InfoEntities/RoomType.cs
@@ -0,0 +1,22 @@
+﻿using StudentLifeHelper.Data.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Data.Entities.InfoEntities
+{
+    [Table("info.info_room_type")]
+    public class RoomType:BaseInfoEntity
+    {
+        [Required]
+        [Column("info_table_id")]
+        public int InfoTableId { get; set; }
+
+        [ForeignKey(nameof(InfoTableId))]
+        public virtual InfoTable? InfoTable { get; set; }
+    }
+}
