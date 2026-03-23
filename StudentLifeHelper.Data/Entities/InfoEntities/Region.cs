40dfaac1b6a0d2258970153c5cd7046ee2da4de6
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-23 23:50:00 +0900

    Info Region added

---
+++ StudentLifeHelper.Data/Entities/InfoEntities/Region.cs
@@ -0,0 +1,23 @@
+﻿using StudentLifeHelper.Data.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Data.Entities.InfoEntities
+{
+    [Table("info.info_region")]
+    public class Region : BaseInfoEntity
+    {
+        [Required]
+        [Column("info_table_id")]
+        public int InfoTableId { get; set; }
+
+
+        [ForeignKey(nameof(InfoTableId))]
+        public virtual InfoTable? InfoTable { get; set; } 
+    }
+}
