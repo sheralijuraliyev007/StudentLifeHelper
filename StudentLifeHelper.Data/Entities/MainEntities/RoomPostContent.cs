67b6ad1733aff5d29e72803c995f28a6ad658d82
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-24 01:56:56 +0900

    RoomPostContent added

---
+++ StudentLifeHelper.Data/Entities/MainEntities/RoomPostContent.cs
@@ -0,0 +1,52 @@
+﻿using StudentLifeHelper.Data.Entities.BaseEntities;
+using StudentLifeHelper.Data.Entities.InfoEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Data.Entities.MainEntities
+{
+
+    [Table("room_post_contents")]
+    public class RoomPostContent : BaseCommonEntity
+    {
+        [Required]
+        [Column("id")]
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [Column("room_post_id")]
+        public long RoomPostId { get; set; }
+
+
+        [ForeignKey(nameof(RoomPostId))]
+        public virtual RoomPost? RoomPost { get; set; }
+
+
+        [Required]
+        [Column("content_id")]
+        public long ContentId { get; set; }
+
+        [ForeignKey(nameof(ContentId))]
+        public virtual Content? Content { get; set; }
+
+
+        [Required]
+        [Column("is_cover")]
+        public bool IsCover { get; set; }
+
+
+        [Required]
+        [Column("status_id")]
+        public int StatusId { get; set; }
+
+        [ForeignKey(nameof(StatusId))]
+        public virtual Status? Status { get; set; }
+
+    }
+}
