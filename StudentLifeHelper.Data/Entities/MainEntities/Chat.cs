1f0d9fd2e113b175c1b8602f079692ff521e02f0
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-24 01:52:43 +0900

    Chats added

---
+++ StudentLifeHelper.Data/Entities/MainEntities/Chat.cs
@@ -0,0 +1,32 @@
+﻿using StudentLifeHelper.Data.Entities.BaseEntities;
+using StudentLifeHelper.Data.Entities.InfoEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Data.Entities.MainEntities
+{
+
+    [Table("chats")]
+    public class Chat : BaseCommonEntity
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public Guid Id { get; set; }
+
+
+        [Required]
+        [Column("status_id")]
+        public int StatusId { get; set; }
+
+
+        [ForeignKey(nameof(StatusId))]
+        public virtual Status? Status { get; set; }
+
+    }
+}
