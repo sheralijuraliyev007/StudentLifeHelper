1f02b49ae5194d7385d4e8f7c3e62650d4bdf62c
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-24 01:04:10 +0900

    CurrencyPost table added

---
+++ StudentLifeHelper.Data/Entities/MainEntities/CurrencyPost.cs
@@ -0,0 +1,71 @@
+﻿using StudentLifeHelper.Data.Entities.BaseEntities;
+using StudentLifeHelper.Data.Entities.InfoEntities;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace StudentLifeHelper.Data.Entities.MainEntities
+{
+
+    [Table("currency_posts")]
+    public class CurrencyPost : BaseCommonEntity
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public long Id { get; set; }
+
+
+        [Required]
+        [Column("title")]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty!;
+
+
+        [Required]
+        [Column("description")]
+        [MaxLength(4000)]
+        public string Description { get; set; } = string.Empty!;
+
+
+        [Required]
+        [Column("user_id")]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User? User { get; set; }
+
+
+        [Required]
+        [Column("from_currency_id")]
+        public int FromCurrencyId { get; set; }
+
+        public virtual CurrencyType? FromCurrencyType { get; set; }
+
+        [Required]
+        [Column("to_currency_id")]
+        public int ToCurrencyId { get; set; }
+
+        public virtual CurrencyType? ToCurrencyType { get; set; }
+
+
+        [Required]
+        [Column("amount")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [Column("rate")]
+        public decimal Rate { get; set; }
+
+
+        [Required]
+        [Column("status_id")]
+        public int StatusId { get; set; }
+
+        [ForeignKey(nameof(StatusId))]
+        public virtual Status? Status { get; set; }
+
+
+    }
+}
