eddcfa71ea8eaa94032a73296079c742167e06cf
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-23 22:22:55 +0900

    Info Country added

---
+++ StudentLifeHelper.Data/Entities/InfoEntities/Country.cs
@@ -0,0 +1,21 @@
+﻿using StudentLifeHelper.Data.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Data.Entities.InfoEntities
+{
+    [Table("info.info_country")]
+    public class Country : BaseInfoEntity
+    {
+        [Required]
+        [Column("info_table_id")]
+        public int InfoTableId { get; set; }
+
+        public virtual InfoTable? InfoTable { get; set; }
+    }
+}
