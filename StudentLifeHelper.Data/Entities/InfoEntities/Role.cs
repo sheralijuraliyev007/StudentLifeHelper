a26acc6b4de6c2e552858470d96c5a8359de2676
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-23 22:33:26 +0900

    Info Table added

---
+++ StudentLifeHelper.Data/Entities/InfoEntities/Role.cs
@@ -0,0 +1,23 @@
+﻿using StudentLifeHelper.Data.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Data.Entities.InfoEntities
+{
+    [Table("info.info_role")]
+    public class Role : BaseInfoEntity
+    {
+        [Required]
+        [Column("info_table_id")]
+        public int InfoTableId { get; set; }
+
+        public virtual InfoTable? InfoTable { get; set; }
+
+
+    }
+}
