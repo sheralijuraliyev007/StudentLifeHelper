c4a3b5dfc8b759bab66c347ee6fc808e0bdfda9c
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-23 22:31:38 +0900

    Info Gender added

---
+++ StudentLifeHelper.Data/Entities/InfoEntities/Gender.cs
@@ -0,0 +1,26 @@
+﻿using StudentLifeHelper.Data.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Data.Entities.InfoEntities
+{
+
+    [Table("info.info_gender")]
+    public class Gender: BaseInfoEntity
+    {
+
+        [Required]
+        [Column("info_table_id")]
+        public int InfoTableId { get; set; }
+
+
+        [ForeignKey(nameof(InfoTableId))]
+        public virtual InfoTable? InfoTable { get; set; }
+
+    }
+}
