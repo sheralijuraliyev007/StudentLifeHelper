b2d3752f0f636726c07e48aaf07b651b27775c11
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-25 02:30:07 +0900

    Interface Repositories added

---
+++ StudentLifeHelper.Data/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Data.Repositories.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        IQueryable<T> GetAll(params Expression<Func<T, object>>[] includes);
+
+        IQueryable<T> GetAllWithNestedIncludes(
+            params string[] includesPath
+            );
+
+        Task<T?> GetById<TK>(TK id);
+        Task Add(T entity);
+
+        Task Update(T entity);
+
+        Task Delete(T entity);
+
+        Task AddRangeAsync(List<T> entities);
+        void UpdateRange(List<T> entities);
+
+        void DeleteRange(List<T> entities);
+
+        Task SaveChanges();
+    }
+}
---
+++ StudentLifeHelper.Data/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using StudentLifeHelper.Data.Entities.InfoEntities;
+using StudentLifeHelper.Data.Entities.MainEntities;
+
+namespace StudentLifeHelper.Data.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IBaseRepository<ContentType> ContentTypeRepository();
+        IBaseRepository<Country> CountryRepository();
+        IBaseRepository<CurrencyType> CurrencyTypeRepository();
+        IBaseRepository<Gender> GenderRepository();
+        IBaseRepository<InfoTable> InfoTableRepository();
+        IBaseRepository<Region> RegionRepository();
+        IBaseRepository<Role> RoleRepository();
+        IBaseRepository<RoomPostType> RoomPostTypeRepository();
+        IBaseRepository<RoomType> RoomTypeRepository();
+        IBaseRepository<Status> StatusRepository();
+        IBaseRepository<Chat> ChatRepository();
+        IBaseRepository<Content> ContentRepository();
+        IBaseRepository<CurrencyPost> CurrencyPostRepository();
+        IBaseRepository<Message> MessageRepository();
+        IBaseRepository<RoomPost> RoomPostRepository();
+        IBaseRepository<RoomPostContent> RoomPostContentRepository();
+        IBaseRepository<User> UserRepository();
+        IBaseRepository<UserChat> UserChatRepository();
+
+        Task SaveChanges();
+        IDbContextTransaction BeginTransaction();
+        IDbContextTransaction? CurrentTransaction();
+    }
+}
