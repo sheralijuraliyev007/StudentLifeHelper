020f41f29076d3273b006d8edc643f271ddb67f2
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-26 10:03:44 +0900

    AppDbContext DbSets are added

--- StudentLifeHelper.Data/Context/AppDbContext.cs
+++ StudentLifeHelper.Data/Context/AppDbContext.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using StudentLifeHelper.Data.Entities.InfoEntities;
 using StudentLifeHelper.Data.Entities.MainEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mime;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StudentLifeHelper.Data.Context
 {
@@ -15,13 +9,26 @@ namespace StudentLifeHelper.Data.Context
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-
-
-        public DbSet<User> Users { get; set; }
-
         public DbSet<ContentType> ContentTypes { get; set; }
-        
         public DbSet<Country> Countries { get; set; }
+        public DbSet<CurrencyType> CurrencyTypes { get; set; }
+        public DbSet<Gender> Genders { get; set; } 
+        public DbSet<InfoTable> InfoTables { get; set; }
+        public DbSet<Region> Regions{ get; set; }
+        public DbSet<Role> Roles{ get; set; } 
+        public DbSet<RoomPostType> RoomPostTypes{ get; set; }
+        public DbSet<RoomType> RoomTypes{ get; set; } 
+        public DbSet<State> States{ get; set; } 
+        public DbSet<Status> Status{ get; set; } 
+        public DbSet<Chat> Chats{ get; set; } 
+        public DbSet<Content> Contents{ get; set; } 
+        public DbSet<CurrencyPost> CurrencyPosts{ get; set; } 
+        public DbSet<Message> Messages{ get; set; } 
+        public DbSet<RoomPost> RoomPosts{ get; set; } 
+        public DbSet<RoomPostContent> RoomPostContents{ get; set; } 
+        public DbSet<User> Users{ get; set; } 
 
+       public DbSet<UserChat> UserChats{ get; set; }
 
     }
+}
