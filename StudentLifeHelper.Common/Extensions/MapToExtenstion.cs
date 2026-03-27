79e017cf9a4b8c3d23646bf84d10fa8614b6096b
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-27 11:02:34 +0900

    MapToEntity extension added

---
+++ StudentLifeHelper.Common/Extensions/MapToExtenstion.cs
@@ -0,0 +1,20 @@
+﻿using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Common.Extensions
+{
+    public static class MapToExtenstion
+    {
+        public static TEntity MapToEntity<TEntity, TModel>(this TModel model, TypeAdapterConfig? config = null)
+        {
+            var entity = config is null ? model.Adapt<TEntity>()
+                : model.Adapt<TEntity>(config);
+            return entity;
+        }
+    }
+}
---
+++ StudentLifeHelper.Service/Admin/Base/BaseInfoService.cs
@@ -0,0 +1,39 @@
+﻿using StatusGeneric;
+using StudentLifeHelper.Data.Entities.InfoEntities;
+using StudentLifeHelper.Data.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Service.Admin.Base
+{
+    public class BaseInfoService<TEntity>(IBaseRepository<TEntity> baseRepository) : StatusGenericHandler, IBaseInfoService<TEntity>
+        where TEntity : class, IHasState
+    {
+        public Task<string> Create<TModel>(TModel model)
+        {
+            var entity = model.MapToEntity
+        }
+
+        public Task<string?> DeleteById<TId>(TId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TDto>> GetAll<TDto>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TDto?> GetById<TDto, TId>(TId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string?> Update<TId, TModel>(TId id, TModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
