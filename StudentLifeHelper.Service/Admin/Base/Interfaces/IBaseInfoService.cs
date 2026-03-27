73d3b928442f879e8d61733cb5e6af440e612e72
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-27 10:30:36 +0900

    IBaseInfoService added

---
+++ StudentLifeHelper.Service/Admin/Base/Interfaces/IBaseInfoService.cs
@@ -0,0 +1,14 @@
+﻿
+using StatusGeneric;
+
+namespace StudentLifeHelper.Service.Admin;
+
+public interface IBaseInfoService<TEntity> : IStatusGeneric
+{
+    Task<List<TDto>> GetAll<TDto>();
+    Task<TDto?> GetById<TDto, TId>(TId id);
+    Task<string> Create<TModel>(TModel model);
+    Task<string?> Update<TId,TModel>(TId id, TModel model);
+
+    Task<string?> DeleteById<TId>(TId id);
+}
