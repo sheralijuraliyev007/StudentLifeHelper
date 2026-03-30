17986d4724212924732b8a3c2bc91c84aa95b229
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-31 02:20:36 +0900

    UserHelper added

---
+++ StudentLifeHelper.Service/Common/Interfaces/IUserHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Service.Common.Interfaces
+{
+    public interface IUserHelper
+    {
+        string GetUserId();
+
+        string GetUsername();
+
+        string GetUserRole();
+        int GetUserRoleId();
+    }
+}
---
+++ StudentLifeHelper.Service/Common/UserHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using StudentLifeHelper.Service.Common.Interfaces;
+using System.Security.Claims;
+
+
+namespace StudentLifeHelper.Service.Common
+{
+
+    public class UserHelper(IHttpContextAccessor httpContextAccessor) : IUserHelper
+    {
+        public string GetUserId() => httpContextAccessor.HttpContext!.User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+        public string GetUsername() => httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Name)!.Value;
+
+        public string GetUserRole() => httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Role)!.Value;
+
+        public int GetUserRoleId() => int.Parse(httpContextAccessor.HttpContext.User.FindFirst("role_id")!.Value);
+    }
+}
