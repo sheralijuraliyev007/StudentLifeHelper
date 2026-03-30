5e26598ee4040e5c846b94c6dc0b614748562760
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-03-31 02:07:16 +0900

    BasicAuthenticationHandler added

---
+++ StudentLifeHelper.Service/Auth/Handlers/BasicAuthenticationHandler.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic;
+using StudentLifeHelper.Common.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace StudentLifeHelper.Service.Auth.Handlers
+{
+    public class BasicAuthenticationHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        IConfiguration configuration, ISystemClock clock)
+        :AuthenticationHandler<AuthenticationSchemeOptions>(options, logger,encoder,clock)
+        
+    {
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("Missing Authorization Header");
+            }
+
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':', 2);
+                var username = credentials[0];
+                var password = credentials[1];
+
+                var schemeName = Scheme.Name;
+
+                var storedUsername = configuration[$"BasicAuth:Schemes:{schemeName}:Username"];
+                var storedPassword = configuration[$"BasicAuth:Schemes:{schemeName}:Password"];
+
+                if (string.IsNullOrEmpty(storedUsername) || string.IsNullOrEmpty(storedPassword) || username != storedUsername || password != storedPassword)
+                {
+                    return AuthenticateResult.Fail("Invalid username or password");
+                }
+
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.Role, RoleConstants.AdminRoleFullName)
+                };
+
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                return AuthenticateResult.Success(ticket);
+
+
+            }
+
+            catch
+            {
+                return AuthenticateResult.Fail("Invalid Authorization Header");
+            }
+        }
+    }
+}
