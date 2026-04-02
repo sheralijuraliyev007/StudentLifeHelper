04f8f49975fce8927970e7080dfef3b09d2fd0c7
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-04-02 14:47:02 +0900

    MinIO service configuration is added

--- StudentLifeHelper.Api/Program.cs
+++ StudentLifeHelper.Api/Program.cs
@@ -1,3 +1,9 @@
+using StudentLifeHelper.Data.Repositories;
+using StudentLifeHelper.Data.Repositories.Interfaces;
+using StudentLifeHelper.Service.Admin;
+using StudentLifeHelper.Service.Admin.Base;
+using StudentLifeHelper.Service.Auth;
+using StudentLifeHelper.Service.Auth.Interfaces;
 using StudentLifeHelper.Service.Infrastructure;
 using StudentLifeHelper.Service.Infrastructure.Interfaces;
 using StudentLifeHelper.Service.Public.Content;
@@ -12,11 +18,19 @@ builder.Services.AddSwaggerGen();
 
 
 
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped(typeof(IBaseInfoService<>), typeof(BaseInfoService<>));
 builder.Services.AddScoped<IMinioService, MinIOService>();
 builder.Services.AddScoped<IContentService, ContentService>();
 builder.Services.AddScoped<IContentService, ContentService>();
 builder.Services.AddScoped<IContentService, ContentService>();
 builder.Services.AddScoped<IContentService, ContentService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<JwtService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
 
 
 
