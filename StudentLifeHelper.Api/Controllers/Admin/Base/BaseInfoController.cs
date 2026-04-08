373c3f3f8adce34029b470126698e2a080c953e8
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-04-08 13:04:04 +0900

    BaseInfoController finished

--- StudentLifeHelper.Api/Controllers/Admin/Base/BaseInfoController.cs
+++ StudentLifeHelper.Api/Controllers/Admin/Base/BaseInfoController.cs
@@ -5,12 +5,13 @@ using StudentLifeHelper.Service.Admin;
 
 namespace StudentLifeHelper.Api.Controllers.Admin.Base
 {
-    public abstract class BaseInfoController<TEntity, TCreateModel,TUpdateModel,TDto,TId>(IBaseInfoService<TEntity> service) : BaseAdminController
+    public abstract class BaseInfoController<TEntity, TCreateModel, TUpdateModel, TDto, TId>(IBaseInfoService<TEntity> service) : BaseAdminController
         where TEntity : class
     {
         [HttpGet]
         [Authorize(Roles = "Administrator")]
-        public async Task<IActionResult> GetAll() { 
+        public async Task<IActionResult> GetAll()
+        {
             var result = await service.GetAll<TDto>();
             if (service.IsValid)
             {
@@ -19,5 +20,54 @@ namespace StudentLifeHelper.Api.Controllers.Admin.Base
             return BadRequest();
         }
 
+        [HttpGet]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> GetById(TId id)
+        {
+            var result = await service.GetById<TDto, TId>(id);
+            if (service.IsValid)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Create(TCreateModel model)
+        {
+            var result = await service.Create(model);
+            if (service.IsValid)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Update(TId id, TUpdateModel model)
+        {
+            var result = await service.Update(id, model);
+            if (service.IsValid)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Delete(TId id)
+        {
+            var result = await service.DeleteById(id);
+            if (service.IsValid)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+
+        }
     }
 }
