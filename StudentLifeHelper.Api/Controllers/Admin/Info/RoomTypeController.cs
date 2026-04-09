ab817f4652a657263eb758a582fb206ee9944771
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-04-09 19:20:23 +0900

    RoomTypeController added

---
+++ StudentLifeHelper.Api/Controllers/Admin/Info/RoomTypeController.cs
@@ -0,0 +1,12 @@
+﻿using StudentLifeHelper.Api.Controllers.Admin.Base;
+using StudentLifeHelper.Common.Dtos.Info;
+using StudentLifeHelper.Common.Models.Info;
+using StudentLifeHelper.Data.Entities.InfoEntities;
+using StudentLifeHelper.Service.Admin;
+
+namespace StudentLifeHelper.Api.Controllers.Admin.Info
+{
+    public class RoomTypeController(IBaseInfoService<RoomType> service) : BaseInfoController<RoomType, BaseInfoCreateModelWithTableId, BaseInfoUpdateModel, InfoDto, int>(service)
+    {
+    }
+}
