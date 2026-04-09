1a2475afd857bad6031e936a3489b4511e97ddb2
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-04-09 18:44:39 +0900

    InfoTableController added

---
+++ StudentLifeHelper.Api/Controllers/Admin/Info/InfoTableController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentLifeHelper.Api.Controllers.Admin.Base;
+using StudentLifeHelper.Common.Dtos.Info;
+using StudentLifeHelper.Common.Models.Info;
+using StudentLifeHelper.Data.Entities.InfoEntities;
+using StudentLifeHelper.Service.Admin;
+
+namespace StudentLifeHelper.Api.Controllers.Admin.Info
+{
+    public class InfoTableController(IBaseInfoService<InfoTable> service) : BaseInfoController<InfoTable, BaseInfoCreateModel, BaseInfoUpdateModel, InfoDto, int>(service)
+    {
+    }
+}
