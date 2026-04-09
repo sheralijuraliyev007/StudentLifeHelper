dbc32c0166d3f58749b2cd6601654f4182ffe280
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-04-09 19:13:42 +0900

    RoomPostTypeController added

---
+++ StudentLifeHelper.Api/Controllers/Admin/Info/RoomPostTypeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentLifeHelper.Api.Controllers.Admin.Base;
+using StudentLifeHelper.Common.Dtos.Info;
+using StudentLifeHelper.Common.Models.Info;
+using StudentLifeHelper.Data.Entities.InfoEntities;
+using StudentLifeHelper.Service.Admin;
+
+namespace StudentLifeHelper.Api.Controllers.Admin.Info
+{
+    public class RoomPostTypeController(IBaseInfoService<RoomPostType> service) : BaseInfoController<RoomPostType, BaseInfoCreateModelWithTableId, BaseInfoUpdateModel, InfoDto, int>(service)
+    {
+    }
+}
