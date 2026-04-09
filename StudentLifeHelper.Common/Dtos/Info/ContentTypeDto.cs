b0b185c8a620576ce45914f9c1b2d75dd3e9779a
sheralijuraliyev007 <jsherali2007@gmail.com>
2026-04-09 18:14:09 +0900

    ContentTypeController added

--- StudentLifeHelper.Common/Dtos/Info/ContentTypeDto.cs
+++ StudentLifeHelper.Common/Dtos/Info/ContentTypeDto.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace StudentLifeHelper.Common.Dtos.Info
 {
-    public class InfoDto : BaseInfoDto
-    { 
+    public class ContentTypeDto : InfoDto
+    {
+        public string TypeName { get; set; }
     }
 }
